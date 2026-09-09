09661230ea8300a8322f19f65df1f577050f8cff
tjnofrrqpj935 <tjnofrrqpj935@atomicmail.io>
2026-09-09 00:08:35 -0400

    Update open-next.config.ts

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,5 +1,2 @@
-import type { OpenNextConfig } from "@opennextjs/cloudflare";
-
-const config: OpenNextConfig = {};
-
-export default config;
+import { defineCloudflareConfig}from"@opennextjs/cloudflare";
+export default defineCloudflareConfig();
