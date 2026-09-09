d5c55a6b877a3d2e08a5948acc9e03c5bd46fdab
tjnofrrqpj935 <tjnofrrqpj935@atomicmail.io>
2026-09-08 23:30:53 -0400

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,4 +1,13 @@
-export default function Home(){
+import { supabase } from "@/lib/supabase"
+
+
+export default async function Home(){
+
+  const { data: products } =
+    await supabase
+    .from("products")
+    .select("*")
+
 
   return (
 
@@ -8,12 +17,32 @@ export default function Home(){
         虚拟商品商城
       </h1>
 
-      <p>
-        商品即将上线
-      </p>
+
+      {
+        products?.map(product => (
+
+          <div key={product.id}>
+
+            <h2>
+              {product.name}
+            </h2>
+
+            <p>
+              {product.description}
+            </p>
+
+            <p>
+              价格：
+              {product.price}
+            </p>
+
+          </div>
+
+        ))
+      }
+
 
     </main>
 
   )
-
 }
